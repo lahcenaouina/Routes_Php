723a20348c5301573920178c218e8211334e4cae
lahcen aouina <107632069+lahcenaouina@users.noreply.github.com>
2023-05-27 09:41:03 +0100

    Update index.php
    Add comments

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
-
 require "vendor/autoload.php";
+// Including the autoload file from the vendor directory, which allows loading classes automatically.
 
+// Declare a new Router object
 $route = new classes\Router();
 
+// Examples of defining routes for different HTTP methods (GET, POST)
 $route
-        ->Get("/",[classes\Home::class , 'index'])
-        ->Get("/about",[classes\About::class , 'index'])
-        ->Get("/about/create",[classes\About::class , 'getter'])
-        ->Post("/about/create",[classes\About::class , 'poster'])
-        ->Post("/lahcen",[classes\About::class , 'lahcen']);
-        
-        echo "<pre>POST";
-        var_dump($_POST) ;
-        echo "</pre>";
-        echo "<pre>GET";
-        var_dump($_GET);
-        echo "</pre>";
-echo $route->Run($_SERVER["REQUEST_URI"] , strtolower($_SERVER["REQUEST_METHOD"]));
+    ->Get("/", [classes\Home::class, 'index']) // Define a GET route for the root URL
+    ->Get("/about", [classes\About::class, 'index']) // Define a GET route for the "/about" URL
+    ->Get("/about/create", [classes\About::class, 'getter']) // Define a GET route for the "/about/create" URL
+    ->Post("/about/create", [classes\About::class, 'poster']) // Define a POST route for the "/about/create" URL
+    ->Post("/Contact", function() {  // Define a POST route for the "/Contact" URL with an anonymous function
+        return "CONTACT";
+    })
+    ->Post("/lahcen", [classes\About::class, 'lahcen']); // Define a POST route for the "/lahcen" URL
+
+// Run the router and echo the result of the matched route
+echo $route->Run($_SERVER["REQUEST_URI"], strtolower($_SERVER["REQUEST_METHOD"]));
+
+// Display the contents of the $_POST superglobal array
+echo "<pre>POST";
+var_dump($_POST);
+echo "</pre>";
+
+// Display the contents of the $_GET superglobal array
+echo "<pre>GET";
+var_dump($_GET);
+echo "</pre>";
+
+
 
